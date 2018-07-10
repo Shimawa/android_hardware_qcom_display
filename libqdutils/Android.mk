2cbba2f7f013170af05c4363c36150da56d9bba8
MkfsSion <myychina28759@gmail.com>
2018-07-10 18:45:17 +0800

    asdfas

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -5,24 +5,34 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
 LOCAL_VENDOR_MODULE           := true
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice libhardware
+LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\" -Wno-float-conversion
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := display_config.h mdp_version.h
 LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
                                  idle_invalidator.cpp \
                                  comptype.cpp qd_utils.cpp \
-                                 cb_utils.cpp  display_config.cpp \
-                                 cb_swap_rect.cpp
+                                 cb_utils.cpp cb_swap_rect.cpp
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES          := $(common_libs) libbinder libqservice libqdutils
+LOCAL_C_INCLUDES                := $(common_includes)
+LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
+LOCAL_SRC_FILES                 := display_config.cpp
+LOCAL_CFLAGS                    := $(common_flags)
+LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayConfig\"
+LOCAL_MODULE_TAGS               := optional
+LOCAL_MODULE                    := libdisplayconfig
+LOCAL_VENDOR_MODULE             := true
+include $(BUILD_SHARED_LIBRARY)
 
-LOCAL_COPY_HEADERS_TO           := $(common_header_export_path)
-LOCAL_COPY_HEADERS              := qdMetaData.h qd_utils.h
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
+include $(CLEAR_VARS)
+LOCAL_EXPORT_C_INCLUDE_DIRS     := $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES          := liblog libcutils libhardware libutils
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
